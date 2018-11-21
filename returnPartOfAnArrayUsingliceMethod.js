9aaf320949843405f7f64f10447c6fb071e7fd90
Lewis Betances <patience174@hotmail.com>
2018-11-20 22:13:05 -0500

    11

---
+++ returnPartOfAnArrayUsingliceMethod.js
@@ -0,0 +1,11 @@
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+
+
+function sliceArray(anim, beginSlice, endSlice) {
+  // Add your code below this line
+    
+    return anim.slice(beginSlice,endSlice);
+  // Add your code above this line
+}
+
+sliceArray(inputAnim, 1, 3);

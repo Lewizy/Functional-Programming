a950de02282b7e114711eb82b7c0010873cf9132
Lewis Betances <patience174@hotmail.com>
2018-11-27 14:26:20 -0500

    16-sort alphabetically

--- sortMethod.js
+++ sortMethod.js
@@ -1,9 +1,7 @@
 function alphabeticalOrder(arr) {
   // Add your code below this line
-  return arr.sort(function(a,b){
-   return b>a;
-     //return b-a
-  })
+  return arr.sort();
+  
   // Add your code above this line
-}console.log(
-alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
+}
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);

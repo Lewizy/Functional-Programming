2a3cfe707b5fd9ad06f6847133e8c8042f49b9b7
Lewis Betances <patience174@hotmail.com>
2018-11-28 22:43:13 -0500

    21

---
+++ convertStringsToURLSlugs.js
@@ -0,0 +1,15 @@
+// the global variable
+var globalTitle = "   Winter Is   Coming  2  ";
+
+// Add your code below this line
+function urlSlug(title) {
+  
+  return title.toLowerCase().trim().split(/\s+/).filter((x) => x !== "").join("-");
+  //filter should always be in between split and join
+  
+  //return title.toLowerCase().trim().split(/\s+/).join('-');
+
+}
+// Add your code above this line
+var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
+console.log(winterComing) winter-is-coming-2

5c976b7a73b2e5591ad8d416789e2bb3f2b20406
Lewis Betances <patience174@hotmail.com>
2018-11-29 03:08:09 -0500

    23

--- introToCurrying&PatialApplication.js
+++ introToCurrying&PatialApplication.js
@@ -1,9 +1,10 @@
-Functional Programming: Introduction to Currying and Partial Application
-The arity of a function is the number of arguments it requires. Currying a function means to convert a function of N arity into N functions of arity 1.
+// Functional Programming: Introduction to Currying and Partial Application
+// The arity of a function is the number of arguments it requires. 
+// Currying a function means to convert a function of N arity into N functions of arity 1.
 
-In other words, it restructures a function so it takes one argument, then returns another function that takes the next argument, and so on.
+// In other words, it restructures a function so it takes one argument, then returns another function that takes the next argument, and so on.
 
-Here's an example:
+// Here's an example:
 
 //Un-curried function
 function unCurried(x, y) {
@@ -17,14 +18,15 @@ function curried(x) {
   }
 }
 curried(1)(2) // Returns 3
-This is useful in your program if you can't supply all the arguments to a function at one time. You can save each function call into a variable, which will hold the returned function reference that takes the next argument when it's available. Here's an example using the curried function in the example above:
+//This is useful in your program if you can't supply all the arguments to a function at one time. You can save each function call into a variable, which will hold the returned function reference that takes the next argument when it's available. Here's an example using the curried function in the example above:
 
 // Call a curried function in parts:
 var funcForY = curried(1);
 console.log(funcForY(2)); // Prints 3
-Similarly, partial application can be described as applying a few arguments to a function at a time and returning another function that is applied to more arguments.
+//Similarly, partial application can be described as applying a few arguments to a function at a time 
+//and returning another function that is applied to more arguments.
 
-Here's an example:
+//Here's an example:
 
 //Impartial function
 function impartial(x, y, z) {
@@ -33,7 +35,7 @@ function impartial(x, y, z) {
 var partialFn = impartial.bind(this, 1, 2);
 partialFn(10); // Returns 13
 
-Fill in the body of the add function so it uses currying to add parameters x, y, and z.
+//Fill in the body of the add function so it uses currying to add parameters x, y, and z.
 
 function add(x) {
   // Add your code below this line
